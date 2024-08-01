f88d910d24ffa13790a5c90de173f4a5833d02cd
Zephy Koji <ldwglindner@gmail.com>
2024-08-01 16:51:37 +0200

    update select animations

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-console.log(initialize);
+let playerState = "idle";
+const dropdown = document.getElementById("animations");
+dropdown?.addEventListener("change", function (e) {
+  playerState = e.target.value;
+});
 // Initialisation Canvas
 function initialize() {
   const canvas = document.getElementById("mycanvas");
@@ -8,10 +12,67 @@ function initialize() {
   const CANVAS_HEIGHT = (canvas.height = 600);
   const spriteWidth = 575;
   const spriteHeight = 523;
-  let frameX = 1;
-  let frameY = 2;
+
   let gameFrame = 0;
-  const staggerFrames = 5;
+  const staggerFrames = 6;
+  const spriteAnimations = [];
+
+  // Array Sprite Animations
+
+  const animationStates = [
+    {
+      name: "idle",
+      frames: 7,
+    },
+    {
+      name: "jump",
+      frames: 7,
+    },
+    {
+      name: "fall",
+      frames: 7,
+    },
+    {
+      name: "run",
+      frames: 7,
+    },
+    {
+      name: "dizzy",
+      frames: 11,
+    },
+    {
+      name: "sit",
+      frames: 5,
+    },
+    {
+      name: "roll",
+      frames: 7,
+    },
+    {
+      name: "bite",
+      frames: 7,
+    },
+    {
+      name: "ko",
+      frames: 12,
+    },
+    {
+      name: "gethit",
+      frames: 4,
+    },
+  ];
+  animationStates.forEach((state, index) => {
+    let frames = {
+      loc: [],
+    };
+    for (let j = 0; j < state.frames; j++) {
+      let positionX = j * spriteWidth;
+      let positionY = index * spriteHeight;
+      frames.loc.push({ x: positionX, y: positionY });
+    }
+    spriteAnimations[state.name] = frames;
+  });
+  console.log(spriteAnimations);
 
   // Create Image and import
   const playerImage = new Image();
@@ -20,11 +81,15 @@ function initialize() {
   function animate() {
     if (ctx === null) return;
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    // ctx.fillRect(50, 50, 100, 100);
+    let position =
+      Math.floor(gameFrame / staggerFrames) %
+      spriteAnimations[playerState].loc.length;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y;
     ctx.drawImage(
       playerImage,
-      frameX * spriteWidth,
-      frameY * spriteHeight,
+      frameX,
+      frameY,
       spriteWidth,
       spriteHeight,
       0,
@@ -32,8 +97,10 @@ function initialize() {
       spriteWidth,
       spriteHeight
     );
-    if (frameX < 6) frameX++;
-    else frameX = 0;
+    if (gameFrame % staggerFrames == 0) {
+      if (frameX < 6) frameX++;
+      else frameX = 0;
+    }
 
     gameFrame++;
 
